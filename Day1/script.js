3cc9df5edb64ebedd859d7278f834d3a7e92accf
kajal-jotwani <kajaljotwani06@gmail.com>
2024-07-14 10:54:24 +0530

    task completed

---
+++ Day1/script.js
@@ -0,0 +1,20 @@
+var myvariable = "hello world";
+console.log(myvariable);
+
+let marks = 100;
+console.log(marks);
+
+const sum = true;
+console.log(sum);
+
+let a = 5;
+let b = true;
+let c = "kajal";
+let d = [2,3,5];
+let e = ["apple", "banana", "cherry"];
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
