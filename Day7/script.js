31483d7201ece2c0c9ba5790e39f307cb48b1da1
kajal-jotwani <kajaljotwani06@gmail.com>
2024-08-18 23:47:03 +0530

    commit

--- Day7/script.js
+++ Day7/script.js
@@ -20,13 +20,40 @@ book.getDetails = function(){
 console.log(book.getDetails());
 
 // Task 4: Add a method to the book object that takes a parameter (year) and updates the book's year property, then log the updated object.
+book.update = function(newYear) {
+    this.Year = newYear;
+};
+book.update(1900);
+console.log(book);
 
 // Task 5: Create a nested object representing a library with properties like name and books (an array of book objects), and log the library object to the console.
+const Library = {
+    name: "centralLibrary",
+    books: [
+        { title: "1984", author: "George Orwell", year: 1949 },
+        { title: "Brave New World", author: "Aldous Huxley", year: 1932 },
+        { title: "Fahrenheit 451", author: "Ray Bradbury", year: 1953 }
+    ]
+};
+console.log(Library);
 
 // Task 6: Access and log the name of the library and the titles of all the books in the library.
+console.log(Library.name);
+Library.books.forEach(book => console.log(book.title))
 
 // Task 7: Add a method to the book object that uses the this keyword to return a string with the book's title and year, and log the result of calling this method.
+book.getTitleAndYear = function() {
+    return `${this.title} (${this.year})`;
+};
+console.log(book.getTitleAndYear());
+
 
 // Task 8: Use a for...in loop to iterate over the properties of the book object and log each property and its value.
+for (let key in book) {
+    console.log(`${key}: ${book[key]}`);
+}
+
 
 // Task 9: Use Object.keys and Object.values methods to log all the keys and values of the book object.
+console.log(Object.keys(book));
+console.log(Object.values(book));
